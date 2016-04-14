514c5245e67d732c3f512a500681ac5054a645e2
XShockinFireX <XShockinFireX@users.noreply.github.com>
2016-04-14 19:40:20 -0400

    Create ExplosiveBottle.php

---
+++ src/ExplosiveBottle/ExplosiveBottle/ExplosiveBottle.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ExplosiveBottle\ExplosiveBottle;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\entity\ThrownExpBottle;
+use pocketmine\Level;
+use pocketmine\level\Explosion;
+use pocketmine\level\Position;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\ProjectileHitEvent;
+
+class Main extends PluginBase implements Listener {
+	
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info(TextFormat::GREEN."ExplosiveBottle Enabled!");
+	}
+	public function onDisable() {
+		$this->getLogger()->info(TextFormat::RED."ExplosiveBottle Disabled!");
+	}
+	public function onProjectileHit(ProjectileHitEvent $event) {
+		$entity = $event->getEntity();
+		if ($entity instanceof ThrownExpBottle) {
+			$theX = $entity->getX();
+			$theY = $entity->getY();
+			$theZ = $entity->getZ();
+			$level = $entity->getLevel();
+			$thePosition = new Position($theX, $theY, $theZ, $level);
+			$theExplosion = new Explosion($thePosition, 5, NULL);
+			$theExplosion->explode();
+            $explosion->explodeB();
+			$level->remove($entity);
+		}
+	}
+}
