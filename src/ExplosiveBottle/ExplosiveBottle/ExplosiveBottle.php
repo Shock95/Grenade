5420d82d06ade16812006c0b431f543be28c48d6
XShockinFireX <XShockinFireX@users.noreply.github.com>
2016-10-01 20:49:38 -0400

    What a noob I was...

--- src/ExplosiveBottle/ExplosiveBottle/ExplosiveBottle.php
+++ src/ExplosiveBottle/ExplosiveBottle/ExplosiveBottle.php
@@ -1,39 +1,34 @@
 <?php
 namespace ExplosiveBottle\ExplosiveBottle;
-use pocketmine\plugin\Plugin;
+
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use pocketmine\entity\ThrownExpBottle;
 use pocketmine\Level;
+use pocketmine\utils\Config;
 use pocketmine\level\Explosion;
 use pocketmine\level\Position;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\ProjectileHitEvent;
+
 class ExplosiveBottle extends PluginBase implements Listener {
 	
 	public function onEnable() {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getLogger()->info(TextFormat::GREEN."ExplosiveBottle Enabled!");
+		$this->getLogger()->info(TextFormat::GREEN. "ExplosiveBottle has been enabled.");
+		$this->saveDefaultConfig();
 	}
-	public function onDisable() {
-		$this->getLogger()->info(TextFormat::RED."ExplosiveBottle Disabled!");
-	}
+	
 	public function onProjectileHit(ProjectileHitEvent $event) {
 		$entity = $event->getEntity();
-		if ($entity instanceof ThrownExpBottle) {
-			$theX = $entity->getX();
-			$theY = $entity->getY();
-			$theZ = $entity->getZ();
-			$level = $entity->getLevel();
-			$thePosition = new Position($theX, $theY, $theZ, $level);
-			$theExplosion = new Explosion($thePosition, 5, NULL);
-			$theExplosion->explodeB();
-            $impact = 1;
-            $damage = 1;
-            
-    }
-    }
+		if($entity instanceof ThrownExpBottle) {
+			$pos = new Position($entity->getX(), $entity->getY(), $entity->getZ(), $entity->getLevel());
+			$explosion = new Explosion($pos, 5, NULL);
+			if($this->getConfig()->get("explode-blocks")) {
+				$explosion->explodeB();
+			} else {
+				$explosion->explodeA();
+			}
+		}
+	}
 }
-    
-          
-        
